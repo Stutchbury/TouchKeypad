9ae67628174314823afdc4434bdb25ec1d438a0a
Philip Fletcher <philip.fletcher@stutchbury.com>
2022-02-24 17:52:38 +0000

    Clean up classes and add more setup methods

--- src/TouchKeypad.h
+++ src/TouchKeypad.h
@@ -10,10 +10,6 @@
  * they do not draw anything on the screen! For that you need the 
  * DisplayTouckKeypad class.
  * 
- * NOTE: This is a very early release (for a specific project) so 
- * assumes a screen of 320x240 and a rotation of 1.
- * I will work on sorting that...
- * 
  * GPLv2 Licence https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  * 
  * Copyright (c) 2022 Philip Fletcher <philip.fletcher@stutchbury.com>
@@ -57,8 +53,6 @@ class TouchKeypad; //Forward declare
 
 /** ******************************************************************************
  *  The class that is the key :-)
-
-
 */
 class TouchKey : public DisplayArea {
 
@@ -68,6 +62,116 @@ class TouchKey : public DisplayArea {
     #else
     typedef void (*TouchKeyFunction) (TouchKey&);
     #endif    
+
+  
+    /**
+     * @brief Construct a new Touch Key object
+     * 
+     */
+    TouchKey(TouchKeypad* keypad, DisplayArea area, uint8_t row, uint8_t col);
+    TouchKey();
+
+    /**
+     * @brief Called by TouchKeypad in the loop/update() 
+     * for each key. This is will 'un-touched' the key
+     */
+    void update();
+
+    /**
+     * @brief Called by TouchKeypad in the loop/update()
+     * for each key only if TSPoint.z is over threshold.
+     */
+    bool update(Coords_s c);
+
+    /**
+     * @brief Set touchDurationMs - normally set only via TouchKeypad
+     * 
+     * @param touchDurationMs 
+     */
+    void setTouchDurationMs(uint16_t touchDurationMs = 100);
+
+    /**
+     * @brief Set touchRepeatMs - normally set via TouchKeypad
+     * 
+     * @param touchRepeatMs 
+     */
+    void setTouchRepeatMs(uint16_t touchRepeatMs = 700);
+
+    /**
+     * @brief Fire the 'touched' event callback function/method
+     * for this key if it has been set.
+     * 
+     * @return true 
+     * @return false 
+     */
+
+    bool fireKeyTouchedCallback();
+
+    /**
+     * @brief Return the ms this key was last touched
+     * 
+     * @return unsigned long 
+     */
+    unsigned long lastTouched();
+
+    /**
+     * @brief Return the ms this key was last 'untouched'
+     * 
+     * @return unsigned long 
+     */
+    unsigned long lastUntouched();
+
+    /**
+     * @brief Return the TouchKey DisplayArea
+     * 
+     * @return DisplayArea 
+     */
+    DisplayArea getTouchKeyArea();
+
+    /**
+     * @brief Set the Touched Handler for this key
+     * 
+     * @param TouchKeyFunction
+     */
+    void setTouchedHandler(TouchKeyFunction f);
+
+    /**
+     * @brief Get the row assigned to the key
+     * 
+     * @return uint8_t 
+     */
+    uint8_t row();
+
+    /**
+     * @brief Get the column assigned to the key 
+     * 
+     * @return uint8_t 
+     */
+    uint8_t col();
+
+    bool enabled();
+
+    void enable(bool e=true);
+
+    /**
+     * Set the key user identifier
+     */
+    void setUserId(unsigned int userId);
+
+    /**
+     * Set the key user state.
+     */    
+    void setUserState(unsigned int userState);
+
+    /**
+     * Get the key user identifier (not unique, defaults to 0)
+     */
+    unsigned int userId();
+
+    /**
+     * Get the key user state.
+     */    
+    unsigned int userState();
 
   private:
     TouchKeypad* keypad;
@@ -83,107 +187,11 @@ class TouchKey : public DisplayArea {
     unsigned int _userState = 0;
     bool _enabled = true;
 
-  
-  public:
-    TouchKey() : DisplayArea(0,0,0,0) {}
-    TouchKey(TouchKeypad* keypad, DisplayArea area, uint8_t row, uint8_t col)
-      : DisplayArea(area), keypad(keypad), _row(row), _col(col)
-    { }
 
 
-    /**
-     * This is the un-touched 'timeout'
-     */
-    void update() {
-      unsigned long now = millis();
-      if ( _isTouched && now > _lastTouched + touchDurationMs) {
-        _isTouched = false;
-        _lastUntouched = now;
-      }
-    }
-
-    /**
-      Called if TSPoint.z is over threshold.
-    */
-    bool update(Coords_s c) {
-      unsigned long now = millis();
-      update();
-      if ( contains(c.x, c.y) ) {
-        if (!_isTouched && now > (_lastTouched + touchRepeatMs) ) {
-          //Only reset touch if untouched by update() or by different key
-          _isTouched = true;
-          _lastTouched = now;
-          return true;
-        }
-      } else {
-        _isTouched = false; //A different key has been touched        
-        _lastUntouched = now;
-      }
-      return false;
-    }
-
-    bool fireKeyTouchedCallback() {
-      if ( touchCallback != NULL ) { 
-        touchCallback(*this);
-        return true;
-      }
-      return false;
-    }
-
-    unsigned long lastTouched() {
-      return _lastTouched;
-    }
-
-    unsigned long lastUntouched() {
-      return _lastUntouched;
-    }
-
-    DisplayArea getTouchKeyArea() {
-      return DisplayArea(x(), y(), w(), h());
-    }
-
-    void setTouchedHandler(TouchKeyFunction f) {
-      touchCallback = f;
-    }
-
-    uint8_t row() {
-      return _row;
-    }
-
-    uint8_t col() {
-      return _col;
-    }
-
-    bool enabled() {
-      return _enabled;
-    }
-
-    void enable(bool e=true) {
-      _enabled = e;
-    }
-
-    /**
-     * Set the key user identifier
-     */
-    void setUserId(unsigned int userId) { _userId = userId; }
-    
-    /**
-     * Set the key user state.
-     */    
-    void setUserState(unsigned int userState) { _userState = userState; }    
-
-    /**
-     * Get the key user identifier (not unique, defaults to 0)
-     */
-    unsigned int userId() { return _userId; }
-
-    /**
-     * Get the key user state.
-     */    
-    unsigned int userState() { return _userState; }    
-    
 };
 
+/** **************************************************************************** */
 /** ******************************************************************************
  *  This is a base class to handle touch and touch callbacks - it won't draw anything
  *  on the display! Use DisplayTouchKeypad to display your
@@ -193,103 +201,131 @@ class TouchKeypad : public DisplayArea {
 
   public:
     /**
-       Constructor
-    */
-    TouchKeypad(TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols)
-      : DisplayArea(keypadArea), ts(touchscreen) { /*, rows(rows), cols(cols)*/
-      initKeys(rows, cols);
-    }
+     * Constructor
+     */
+    TouchKeypad(TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols);
+
+
+
+    void setDisplayArea(DisplayArea area);
+
+    void setDisplayWidth(uint16_t width);
+    void setDisplayHeight(uint16_t height);
+    void setTsTop(uint16_t i);
+    void setTsBottom(uint16_t i);
+    void setTsLeft(uint16_t i);
+    void setTsRight(uint16_t i);
+
+    void update(uint16_t touchUpdateMs = 10);
+
+    /**
+     * @brief Unlike the GFX display, this does not take any arguments, it 
+     * just rotates the touch screen 90 degrees clockwise.
+     * The touchscreen is highly likely to at a completetely different 
+     * native rotation to the actual (display) screen and also be (internally) 
+     * wired back-to-front in x and/or y.
+     * To deal with this we have flipX() & flipY() as below.
+     * If required, call rotate() just once and then use flipX() and/or 
+     * flipY() to match the display.  
+     */
+    void rotate();
+
+    /**
+     * @brief Swap tsLeft & tsRight boundaries
+     */
+    void flipX();
+
+    /**
+     * @brief Swap tsTop & tsBottom boundaries
+     */
+    void flipY();    
+
+    /**
+     * @brief Initialise the keypad with the number of required
+     * rows and columns. Normally done in the constructor but can 
+     * be reset at runtime.
+     * 
+     * @param rows 
+     * @param cols 
+     */
+    void initKeys(uint8_t rows, uint8_t cols);
+
+    /**
+     * @brief Set how long the key stays 'touched'
+     * 
+     * @param touchDurationMs 
+     */
+    void setTouchDurationMs(uint16_t touchDurationMs = 100);
+    
+    /**
+     * @brief Set how frequently the touch event is fired 
+     * if the key is continuously pressed
+     *  
+     * @param touchRepeatMs 
+     */
+    void setTouchRepeatMs(uint16_t touchRepeatMs = 700);
 
 
 
     /**
-     * This is the 'abstract' key touch handler method. By default returns
+     * @brief This is the 'abstract' key touch handler method. By default returns
      * true if the TouchKeyCallback has been fired.
      * Override in subclasses.
+     * 
+     * @param tk 
+     * @return true 
+     * @return false 
      */
-    virtual bool onKeyTouched(TouchKey& tk) {
-      return tk.fireKeyTouchedCallback();
-    }
+    virtual bool onKeyTouched(TouchKey& tk);
 
+    /**
+     * @brief Get a TouchKey based on its row/column position
+     * 
+     * @param row 
+     * @param col 
+     * @return TouchKey& 
+     */
+    TouchKey& key(uint8_t row, uint8_t col);
 
+    /**
+     * @brief Returns true if enabled
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool enabled();
 
-    void update(uint16_t touchUpdateMs = 10) {
-      if ( _enabled ) {
-        now = millis();
-        if ( now > lastTouchUpdate + touchUpdateMs ) {
-          lastTouchUpdate = now;
-          //Get the TSPoint
-          TSPoint p = ts.getPoint();
-          bool haveTouch = false;
-          Coords_s coords = {0, 0};
-          if (p.z > ts.pressureThreshhold) {
-            haveTouch = true;
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            //@TODO This will have to change for screen rotation...
-            coords.x = displayWidth - map(p.x, tsMinX, tsMaxX, 0, displayWidth);
-            coords.y = map(p.y, tsMinY, tsMaxY, 0, displayHeight);
-            //coords = mapTSPoint(p);
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            //Serial.printf("coords c: %i, y: %i \n", coords.x, coords.y);
-          }
-          for ( uint8_t r = 0; r < rows; r++ ) {
-            for ( uint8_t c = 0; c < cols; c++ ) {
-              //Update each key with Coords_s
-              if ( haveTouch && keys[r][c].update(coords) ) {
-                onKeyTouched(keys[r][c]);
-              } else {
-                keys[r][c].update();
-              }
-            }
-          }
-        }
-      }
-    }
-
-    void initKeys(uint8_t rows, uint8_t cols) {
-      this->rows = rows;
-      this->cols = cols;
-      for ( uint8_t r = 0; r < rows; r++ ) {
-        for ( uint8_t c = 0; c < cols; c++ ) {
-          keys[r][c] = TouchKey(this, DisplayArea(xDiv(cols, c), yDiv(rows, r), wDiv(cols), hDiv(rows)), r, c);
-        }
-      }
-    }
-
-
-    TouchKey& key(uint8_t row, uint8_t col) {
-      return keys[row][col];
-    }
-
-    bool enabled() {
-      return _enabled;
-    }
-
-    void enable(bool e) {
-      _enabled = e;
-    }
+    /**
+     * @brief Pass true to enable, false to disable.
+     * 
+     * @param e 
+     */
+    void enable(bool e);
 
     /**
      * Set the keypad user identifier
      */
-    void setUserId(unsigned int userId) { _userId = userId; }
+    void setUserId(unsigned int userId);
     
     /**
      * Set the keypad user state.
      */    
-    void setUserState(unsigned int userState) { _userState = userState; }    
+    void setUserState(unsigned int userState);
 
     /**
      * Get the keypad user identifier (not unique, defaults to 0)
      */
-    unsigned int userId() { return _userId; }
+    unsigned int userId();
 
     /**
      * Get the keypad user state.
      */    
-    unsigned int userState() { return _userState; }    
+    unsigned int userState();
 
   protected:
+
+    enum TouchscreenEdge { TS_TOP, TS_RIGHT, TS_BOTTOM, TS_LEFT };
+    
     TouchScreen& ts;
     uint8_t rows = 0;
     uint8_t cols = 0;
@@ -298,52 +334,28 @@ class TouchKeypad : public DisplayArea {
     uint16_t displayWidth = 320;
     uint16_t displayHeight = 240;
     //@TODO setTouchBounds()
-    uint16_t tsMinX = 109;
-    uint16_t tsMinY = 110;
-    uint16_t tsMaxX = 940;
-    uint16_t tsMaxY = 945;
+
+    /**
+     * These four members define the active area (boundaries) of 
+     * the touchscreen.
+     * We swap these values around with rotate or flipX, flipY
+     * @TODO store and read these from EEPROM
+     */
+    uint16_t tsRight = 940;
+    uint16_t tsTop = 110;
+    uint16_t tsBottom = 945;
+    uint16_t tsLeft = 109;
+    
     unsigned long now = millis();
-    //uint16_t touchUpdateMs = 10;
     unsigned long lastTouchUpdate = 0;
     unsigned int _userId = 0;
     unsigned int _userState = 0;
 
 
-    /**
-       @TODO we may have to mess with this for screen rotation...
-    */
-    Coords_s mapTSPoint(
-      TSPoint p,
-      uint16_t displayWidth,
-      uint16_t displayHeight,
-      uint16_t tsMinX,
-      uint16_t tsMinY,
-      uint16_t tsMaxX,
-      uint16_t tsMaxY
-    ) {
-      return //Coords_s(
-      { (displayWidth - map(p.x, tsMinX, tsMaxX, 0, displayWidth)),
-        (map(p.y, tsMinY, tsMaxY, 0, displayHeight))
-      };
-      //);
-    }
-    /**
-      @TODO
-    */
-    Coords_s mapTSPoint( TSPoint p ) {
-      return mapTSPoint(p, displayWidth, displayHeight, tsMinX, tsMaxX, tsMinY, tsMaxY);
-    }
-
-
-
 };
-
-
 
 #ifdef _ADAFRUIT_GFX_H
 #include "gfx/DisplayTouchKeypad.h"
 #endif
-
-
 
 #endif
--- src/gfx/DisplayTouchKeypad.h
+++ src/gfx/DisplayTouchKeypad.h
@@ -18,9 +18,9 @@
 #ifndef DISPLAY_TOUCH_KEYPAD_H
 #define DISPLAY_TOUCH_KEYPAD_H
 
-#ifndef TOUCH_KEYPAD_H
+#include "Adafruit_GFX.h"
+
 #include "TouchKeypad.h"
-#endif
 
 #if defined(ARDUINO_ARCH_ESP32) || defined(ESP8266) || defined(CORE_TEENSY)
   #include <functional>
@@ -39,6 +39,99 @@ class DisplayTouchKeypad : public TouchKeypad {
     #endif    
 
   
+    /**
+     * Constructor with no font set - either setFont() or all buttons use a draw function
+     */
+    DisplayTouchKeypad(Adafruit_GFX& d, TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols);
+
+    /**
+     * Constructor with a font set
+     */
+    DisplayTouchKeypad(Adafruit_GFX& d, const GFXfont* f, TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols);
+
+
+    /**
+     * Call both update and draw
+     * 
+     * If overriding either update or draw in subclasses, you must override this too (if
+     * you're going to call it) 
+     * 
+     * @param displayRefreshMs 
+     * @param touchUpdateMs 
+     */
+    void updateAndDraw(uint16_t touchUpdateMs = 10, uint16_t displayRefreshMs=100);
+
+    /**
+     * @brief Draw the keys and refresh the touched UI feedback
+     * 
+     * @param displayRefreshMs - the refresh rate to update the display
+     */
+    void draw(uint16_t displayRefreshMs=100);
+
+    /**
+     * @brief Set a label for a specific key. Max 4 chars (plus null)
+     * 
+     * @param row 
+     * @param col 
+     * @param label 
+     */
+    void setLabel(uint8_t row, uint8_t col, char label[5]);
+
+    /**
+     * @brief Set the Background Colour of the keys
+     * 
+     * @param colour 
+     */
+    void setBackgroundColour(uint16_t colour);
+
+    /**
+     * @brief Set the Foreground Colour of the keys
+     * 
+     * @param colour 
+     */
+    void setForegroundColour(uint16_t colour);
+
+    /**
+     * @brief Set the that flashes on touch
+     * 
+     * @param colour 
+     */
+    void setTouchedColour(uint16_t colour);
+
+
+    /**
+     * @brief Get the Label for a key
+     * 
+     * @param row 
+     * @param col 
+     * @return char* 
+     */
+    char* getLabel(uint8_t row, uint8_t col);
+
+    /**
+     * @brief Set a Draw Handler for a a key - if set this will be used
+     * rather than drawing a label.
+     * 
+     * @param row 
+     * @param col 
+     * @param f 
+     */
+    void setDrawHandler(uint8_t row, uint8_t col, TouchKeyDrawFunction f);
+
+    /**
+     * @brief Set the Font for text labels
+     * 
+     * @param f 
+     */
+    void setFont(const GFXfont* f);
+
+    /**
+     * @brief Set the DisplayArea of the DisplayTouchKeypad to the
+     * background colour.
+     * 
+     */
+    void clear();
+
   protected:
     char labels[TOUCH_KEYPAD_MAX_ROWS][TOUCH_KEYPAD_MAX_COLS][5];
     Adafruit_GFX& gfx;
@@ -65,111 +158,7 @@ class DisplayTouchKeypad : public TouchKeypad {
     bool forceRefresh = false;
 
 
-  public:
-    /**
-       Constructor with no font set - either setFont() or al buttons use draw function
-    */
-    DisplayTouchKeypad(Adafruit_GFX& d, TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols)
-      : TouchKeypad(touchscreen, keypadArea, rows, cols), gfx(d) {
-    }
 
-    /**
-       Constructor with font set
-    */
-    DisplayTouchKeypad(Adafruit_GFX& d, const GFXfont* f, TouchScreen& touchscreen, DisplayArea keypadArea, uint8_t rows, uint8_t cols)
-      : TouchKeypad(touchscreen, keypadArea, rows, cols), gfx(d), font(f) {
-    }
-
-
-    /**
-     * Call both update and draw
-     * 
-     * If overriding eith update or draw in supcallses, you must override this too (if
-     * you're going to call it) 
-     * 
-     * @param displayRefreshMs 
-     * @param touchUpdateMs 
-     */
-    void updateAndDraw(uint16_t touchUpdateMs = 10, uint16_t displayRefreshMs=100)  {
-      TouchKeypad::update(touchUpdateMs);
-      draw(displayRefreshMs);
-    }
-
-    void draw(uint16_t displayRefreshMs=100) {
-      if ( _enabled ) {
-        //Do event updates and fire touch handlers
-        now = millis();
-        //redraw as required
-        if ( now > lastDisplayRefresh + displayRefreshMs ) {
-          lastDisplayRefresh = now;
-          if ( forceRefresh ) {
-            gfx.fillRect(x(), y(), w(), h(), bgColour);
-          }
-          int16_t  x, y;
-          uint16_t w, h;
-          for ( uint8_t row = 0; row < rows; row++ ) {
-            for ( uint8_t col = 0; col < cols; col++ ) {
-              if ( key(row, col).enabled() ) {
-                bool keyStale = ( drawnUserId[row][col] != key(row, col).userId() || drawnUserState[row][col] != key(row, col).userState() );
-                drawnUserId[row][col] = key(row, col).userId();
-                drawnUserState[row][col] = key(row, col).userState();
-                //Draw the feedback flash
-                if ( key(row, col).lastTouched() > key(row, col).lastUntouched() ) {
-                  //if ( key(row, col).enabled() ) {
-                    gfx.fillRoundRect(key(row, col).x()+keyMargin, key(row, col).y()+keyMargin, key(row, col).w()-(keyMargin*2), key(row, col).h()-(keyMargin*2), keyRadius, touchedColour);
-                  //}
-                  keyUntouched[row][col] = false;
-                } else if ( forceRefresh || keyStale || (!keyUntouched[row][col] && now > untouchDelayMs + key(row, col).lastTouched() ) ) {
-                  gfx.fillRect(key(row, col).x(), key(row, col).y(), key(row, col).w(), key(row, col).h(), bgColour);
-                  if ( keyLabelFunctions[row][col] != NULL ) {
-                    keyLabelFunctions[row][col](keys[row][col]);
-                  } else {
-                    gfx.drawRoundRect(key(row, col).x()+keyMargin, key(row, col).y()+keyMargin, key(row, col).w()-(keyMargin*2), key(row, col).h()-(keyMargin*2), keyRadius, fgColour);
-                    if ( strlen(labels[row][col]) > 0 ) {
-                      gfx.setFont(font);
-                      gfx.setTextColor(fgColour);
-                      gfx.getTextBounds(labels[row][col], key(row, col).x(), key(row, col).b(), &x, &y, &w, &h);
-                      gfx.setCursor(key(row, col).xCl() - (w / 2), key(row, col).yCl() + (h / 2));
-                      gfx.print(labels[row][col]);
-                    }
-                  }
-                  keyUntouched[row][col] = true;
-                }
-              }
-            }
-          }
-          forceRefresh = false;
-        }
-      }
-    }
-
-
-    void setLabel(uint8_t row, uint8_t col, char label[5]) {
-      strcpy(labels[row][col], label);
-    }
-
-    char* getLabel(uint8_t row, uint8_t col) {
-      return labels[row][col];
-    }
-
-    void setDrawHandler(uint8_t row, uint8_t col, TouchKeyDrawFunction f) {
-      keyLabelFunctions[row][col] = f;
-    }
-
-    void setFont(const GFXfont* f) {
-      if (font == NULL || font != f) {
-        font = f;
-        //gfx.setFont(font);
-      }
-    }
-
-    void enable(bool b=true) {
-      TouchKeypad::enable(b);
-      //@TODO This is probaly not a good idea...
-      gfx.fillRect(x(), y(), w(), h(), bgColour);
-      forceRefresh = b;
-    }
-      
 };
 
 
